f93666e8ba0bfe748e597473a9bb0eee8ced16b6
Test02 <t36t02@gmail.com>
2023-02-21 20:48:05 -0600

    Agregar archivos de proyecto.

---
+++ Ejercicio2/MainPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Ejercicio2
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void Btninformacion_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(nombre.Text) || string.IsNullOrWhiteSpace(apellidos.Text)
+                || string.IsNullOrWhiteSpace(edad.Text) || string.IsNullOrWhiteSpace(correo.Text))
+            {
+                await DisplayAlert("Aviso", "Debe llenar todos los campos", "Ok");
+            }
+            else
+            {
+                var estudiante = new Models.Persona()
+                {
+                    Nombre = nombre.Text,
+                    Apellidos = apellidos.Text,
+                    Edad = edad.Text,
+                    Correo = correo.Text,
+                };
+
+                var paginaSegunda = new Views.Informacion
+                {
+                    BindingContext = estudiante
+                };
+                LimpiarCampos();
+                await Navigation.PushAsync(paginaSegunda);
+            }
+        }
+
+        private void LimpiarCampos()
+        {
+            nombre.Text = "";
+            apellidos.Text = "";
+            edad.Text = "";
+            correo.Text = "";
+        }
+    }
+}
---
+++ Ejercicio2/Models/Persona.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ejercicio2.Models
+{
+    public class Persona
+    {
+        public string Nombre { get; set; }
+
+        public string Apellidos { get; set; }
+
+        public string Edad { get; set; }
+
+        public string Correo { get;set; }
+    }
+}
